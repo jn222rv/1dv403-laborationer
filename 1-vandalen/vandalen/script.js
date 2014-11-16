60362281dbbf9c879d1396c0331dd47e6da42be2
Jonathan Nilsson <jn222rv@student.lnu.se>
2014-11-16 21:43:23 +0000

    Assingment 4 mostly done

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,7 +3,49 @@
 var makePerson = function(persArr){
 
 
-	// Din kod här...
-
+	//console.log(persArr);
+    
+    var number = 0;
+    var namesArr = [];
+    
+    var newObj = {
+        minAge: 0,
+        maxAge: 0,
+        averageAge: 0,
+        names: ""
+    };
+    
+    var compareFun = function compareNumbers(a,b) {
+        return a.age - b.age;
+    }
+    
+    var test = function testFun(element, index, array) {
+        number += array[index].age;
+    }
+    
+    var nameTest = function nameFun(element, index, array) {
+        namesArr.push(array[index].name);
+    } 
+    
+    var compareName = function othercompareFun(a,b) {
+        return a.localeCompare(b);
+    }
+    
+    
+    persArr.forEach(test);
+    persArr.forEach(nameTest);
+    
+    persArr.sort(compareFun);
+    
+    namesArr.sort(compareName);
+    
+    newObj.minAge = persArr[0].age;
+    newObj.maxAge = persArr[(persArr.length)-1].age
+    newObj.averageAge = Math.round(number/persArr.length);
+    newObj.names = namesArr.join(', ');
+    
+    console.log(newObj.names);
+    
+    return newObj;
 }
 

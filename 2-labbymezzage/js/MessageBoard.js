595842d2ef451149c29021edd391ed94a8cda6df
Jonathan Nilsson <jn222rv@student.lnu.se>
2014-11-24 17:30:24 +0000

    Number of Messages are now showing, and most basics are done

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -4,6 +4,7 @@
 var MessageBoard = {
     
     messages: [],
+    numberOfMessages: 0,
     
     init:function(e){
          
@@ -11,14 +12,37 @@ var MessageBoard = {
         
         node.addEventListener("click", function(){
                 
-            var text = node.parentNode.querySelector("input");    
-            MessageBoard.messages.push(text.value);
-            console.log(MessageBoard.messages);
+            var text = node.parentNode.querySelector("input");   
+            
+            if(/\S/.test(text.value))
+            {
+                
+                MessageBoard.messages.push(text.value);
+                
+                console.log(MessageBoard.messages);
+                
+                var newElement = document.createElement("p");
+                var div = document.getElementById("messageArea");
+                
+                newElement.appendChild(document.createTextNode(MessageBoard.messages[MessageBoard.messages.length-1]));
+                div.appendChild(newElement);
+                
+                document.innerHTML = div;
+                
+                MessageBoard.numberOfMessages += 1;
+                MessageBoard.increment();
+            }
             
         });
         
-        
+        MessageBoard.increment();
+                
         console.log("init() is done");
+    },
+    
+    increment:function(){
+        var numberDiv = document.querySelector("#numberOfMessages p");
+        numberDiv.innerHTML = "Antal meddelanden: "+MessageBoard.numberOfMessages;
     }
 }
 

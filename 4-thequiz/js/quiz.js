734e76b074698ac9b9c9294a6120dff68a75ee9f
Jonathan Nilsson <jn222rv@student.lnu.se>
2014-12-15 00:11:06 +0000

    Just Design left

--- 4-thequiz/js/quiz.js
+++ 4-thequiz/js/quiz.js
@@ -1,9 +1,14 @@
+"use strict"
+
 function Quiz()
 {
     var xhr = null;
     var url = "http://vhost3.lnu.se:20080/question/1";
     var object = null;
     var p = null;
+    var wrongMessage = null;
+    var numberOfGuesses = 0;
+    var answer = null;
   
     this.init = function(){
       
@@ -11,19 +16,30 @@ function Quiz()
       
         var form = document.querySelector("form");
       
-        var answer = form.elements["answer"];
+        answer = form.elements["answer"];
         answer.focus();
         
         var div = document.createElement("div");
-        p = document.createElement("p");
+        p = document.getElementById("question");
+        wrongMessage = document.createElement("p");
         p.innerHTML = "";
-        div.appendChild(p);
+        wrongMessage.innerHTML = "";
+        div.appendChild(wrongMessage);
         document.querySelector("body").appendChild(div);
-      
+        
+        var a = document.createElement("a");
+        var img = document.createElement("img");
+        img.src = "img/refresh.png"
+        a.setAttribute("href","#");
+        a.appendChild(img);
+        div.appendChild(a);
+        
+        a.addEventListener("click",restart);
+        
         form.onsubmit = function(e){
             xhr.open("POST",object["nextURL"],true);
             xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(JSON.stringify({answer:"2"}));
+            xhr.send(JSON.stringify({answer:answer.value}));
             return false;
         };
         
@@ -40,8 +56,14 @@ function Quiz()
                 }
                 else
                 {
+                    numberOfGuesses += 1;
                     checkIfCorrect();
                 }
+            }
+            else if(xhr.status === 400)
+            {
+                wrongMessage.innerHTML = "Det var fel!";
+                numberOfGuesses += 1;
             }
         }
             
@@ -57,12 +79,30 @@ function Quiz()
     };
     
     function checkIfCorrect(){
-        if(/Correct/.test(object["message"]))
+        
+        if(object.hasOwnProperty("nextURL"))
         {
-            console.log("jag var fcoreett");
-            xhr.open("GET",object["nextURL"],true);
-            xhr.send(null);
+            if(/Correct/.test(object["message"]))
+            {
+                answer.value = "";
+                answer.focus();
+                xhr.open("GET",object["nextURL"],true);
+                xhr.send(null);
+                wrongMessage.innerHTML = "";
+            }
         }
+        else
+        {
+            wrongMessage.innerHTML = "";
+            p.innerHTML = "Det tog totalt " + numberOfGuesses + " antal gissnignar";
+            document.querySelector("form").elements["button"].disabled = true;
+            answer.value = null;
+            answer.disabled = true;
+        }
+    };
+    
+    function restart(){
+        console.log("restart engae");  
     };
   
 };
